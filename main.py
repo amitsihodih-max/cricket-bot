e4486a56bb6ec5a3d5bc0e66c1dd611063ba9599
amitsihodih-max <amitsihodih@gmail.com>
2026-01-14 03:05:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ CHANNEL_ID = "-1003263006563"
 async def send_message():
     bot = Bot(token=BOT_TOKEN)
     text = get_prediction()
-    await bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=text
-    )
+
+    if text:
+        await bot.send_message(chat_id=CHANNEL_ID, text=text)
 
 async def main():
     while True:
         await send_message()
-        await asyncio.sleep(10800)  # 3 hours
+        await asyncio.sleep(10800)  # every 3 hours
 
 asyncio.run(main())
