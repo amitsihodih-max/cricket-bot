49516dbf74235be7fe5f61c5e64cf750b0dd0a61
amitsihodih-max <amitsihodih@gmail.com>
2026-01-14 02:07:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+from telegram import Bot
+import time
+
+BOT_TOKEN = "8591884031:AAGFIxdI5rLsq_qlLBemgO9O1rNbLyHuxIk"
+CHANNEL_ID = "-1003263006563"
+
+async def send_message():
+    bot = Bot(token=BOT_TOKEN)
+    await bot.send_message(
+        chat_id=CHANNEL_ID,
+        text="🤖 Cloud bot is alive!"
+    )
+
+async def main():
+    while True:
+        await send_message()
+        await asyncio.sleep(3600)  # wait 1 hour
+
+asyncio.run(main())
