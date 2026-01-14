b55df5aa3a09cf96ca306727c6838c06f48cb9ac
amitsihodih-max <amitsihodih@gmail.com>
2026-01-14 09:29:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import asyncio
 from telegram import Bot
-from post_prediction import get_prediction
 
 BOT_TOKEN = "8591884031:AAGFIxdI5rLsq_qlLBemgO9O1rNbLyHuxIk"
 CHANNEL_ID = "-1003263006563"
 
 async def send_message():
     bot = Bot(token=BOT_TOKEN)
-    text = get_prediction()
-
-    if text:
-        await bot.send_message(chat_id=CHANNEL_ID, text=text)
+    await bot.send_message(
+        chat_id=CHANNEL_ID,
+        text="🤖 Cloud bot is alive!"
+    )
 
 async def main():
     while True:
         await send_message()
-        await asyncio.sleep(10800)  # every 3 hours
+        await asyncio.sleep(3600)  # 1 hour
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
